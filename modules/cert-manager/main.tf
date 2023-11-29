34baab22700352f93113124f1097072ac0292c82
Maxim Generalov <generalov.maksim@gmail.com>
2023-11-29 20:38:11 +0000

    refactoring

--- modules/cert-manager/main.tf
+++ modules/cert-manager/main.tf
@@ -3,15 +3,6 @@ resource "kubernetes_namespace_v1" "this" {
     name = var.helm_config.namespace
   }
 }
-resource "kubernetes_service_account_v1" "this" {
-  metadata {
-    name      = local.kubernetes_service_account_name
-    namespace = kubernetes_namespace_v1.this.metadata[0].name
-    annotations = {
-      "iam.gke.io/gcp-service-account" = google_service_account.service_account.email
-    }
-  }
-}
 resource "helm_release" "this" {
   depends_on = [
     kubernetes_namespace_v1.this,
@@ -24,19 +15,23 @@ resource "helm_release" "this" {
   repository = var.helm_config.chart.repository
   version    = var.helm_config.chart.version
   timeout    = 600
-  set {
-    name  = "serviceAccount.name"
-    value = kubernetes_service_account_v1.this.metadata[0].name
-    type  = "string"
-  }
-  values = [file(format("%s/%s",local.environment_dir,var.helm_config.chart.values_file))]
+  values = [
+    yamlencode({
+      serviceAccount = {
+        annotations = {
+          "iam.gke.io/gcp-service-account" = google_service_account.service_account.email
+        }
+      }
+    }),
+    file(format("%s/%s", local.environment_dir, var.helm_config.chart.values_file))
+  ]
 }
 
 resource "kubectl_manifest" "cert-manager-cluster-issuer" {
-  depends_on      = [
+  depends_on = [
     helm_release.this
   ]
-  for_each = var.clusterIssuers
+  for_each        = var.clusterIssuers
   validate_schema = false
   yaml_body = templatefile("${path.module}/manifests/cert-manager-cluster-issuer.tmpl.yml", {
     clusterIssueName        = each.key
